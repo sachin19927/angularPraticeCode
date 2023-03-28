1d70d4a8ff6f3a6e228083ba219d06b1012a57d0
Bindisha <bindishaparmar01@gmail.com>
2023-03-28 17:31:02 +0530

    recent commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerListComponent } from './component/server-list/server-list.component';
+import { DatepipePipe } from './shared/datepipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerListComponent
+    ServerListComponent,
+    DatepipePipe
   ],
   imports: [
     BrowserModule,
