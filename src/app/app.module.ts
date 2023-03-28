3ac9a1b71bed8db1978979dc24ea7f7f7f2aa3b5
sachin19927 <sachin19927@gmail.com>
2023-03-28 18:20:51 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerListComponent } from './component/server-list/server-list.component';
-import { DatepipePipe } from './shared/datepipe.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerListComponent,
-    DatepipePipe
+    ServerListComponent
   ],
   imports: [
     BrowserModule,
